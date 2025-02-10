6ddda04140b30816ccd43c5765081275bb2bada1
jbesse <besse.julien24@gmail.com>
2025-02-10 22:37:28 +0100

    home: create menu for social network

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -6,14 +6,69 @@ const Menu = () => {
     return (
         <nav className="menu">
             <ul>
-                <li><a href="#realisations">Réalisations</a></li>
-                <li><Link to="/projets">Projets</Link></li>
-                <li><a href="https://medium.com/@besse.julien24" target="_blank" rel="noopener noreferrer">Medium</a></li>
-                <li><a href="https://github.com/SuperBesse" target="_blank" rel="noopener noreferrer">GitHub</a></li>
-                <li><a href="#realisations-persos">Réalisations persos</a></li>
+                {/* <li>
+                    <Link to="/" className="menu-icon">
+                        <i className="fas fa-home"></i>
+                    </Link>
+                </li> */}
+                <li>
+                    <a
+                        href="https://github.com/SuperBesse"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="menu-icon"
+                    >
+                        <i className="fab fa-github"></i>
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="https://medium.com/@besse.julien24"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="menu-icon"
+                    >
+                        <i className="fab fa-medium"></i>
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="https://twitter.com/thesuperbesse"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="menu-icon"
+                    >
+                        <i className="fab fa-twitter"></i>
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="https://apps.apple.com/tt/developer/julien-besse/id1117496049?l=fr-FR"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="menu-icon"
+                    >
+                        <i className="fab fa-apple"></i>
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="mailto:jbesse.info@gmail.com"
+                        className="menu-icon"
+                    >
+                        <i className="fas fa-envelope"></i>
+                    </a>
+                </li>
             </ul>
         </nav>
     );
 };
 
 export default Menu;
+
+
+
+
+
+//<li><a href="https://medium.com/@besse.julien24" target="_blank" rel="noopener noreferrer">Medium</a></li>
+//<li><a href="https://github.com/SuperBesse" target="_blank" rel="noopener noreferrer">GitHub</a></li>
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css'
 import App from './App.tsx'
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
